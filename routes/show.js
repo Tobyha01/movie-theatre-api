4567427fb62e57bfdc826b202e98bbc25e4e4ce7
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-28 08:04:55 +0000

    Imported in the required libraries and models from other folders, and created an export

---
+++ routes/show.js
@@ -0,0 +1,9 @@
+const {db} = require("../db")
+const {check, validationResult} = require("express-validator")
+const express = require("express")
+const router = express.Router()
+const {Show} = require("../models/Show")
+
+router.use(express.json())
+
+module.exports = router
