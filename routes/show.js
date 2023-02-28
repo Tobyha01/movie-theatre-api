7e8d9746cb646ab7f327edea5f063177611ed1ce
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-28 08:15:06 +0000

    Changed the require of the 'Show' model from the show.js file to the index.js file located inside the models folder

--- routes/show.js
+++ routes/show.js
@@ -2,7 +2,7 @@ const {db} = require("../db")
 const {check, validationResult} = require("express-validator")
 const express = require("express")
 const router = express.Router()
-const {Show} = require("../models/Show")
+const {Show} = require("../models/index")
 
 router.use(express.json())
 
