0f49f8aeb1eea8fb270cf4ce414b5125e823d538
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-28 08:19:17 +0000

    Rectified a typo on the constant requiring in the User model. Created two get requests, one to fetch all instances (entries) of the model (db), and a second get request to find one instance (entry) of the model (db) specified by the request parameter id.

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,30 @@ const {db} = require("../db")
 const {check, validationResult} = require("express-validator")
 const express = require("express")
 const router = express.Router()
-const {Show} = require("../models/User")
+const {User} = require("../models/index")
 
 router.use(express.json())
+
+router.get("/", async function(request, response) {
+    try{
+        
+        response.status(200).send(await User.findAll())
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+router.get("/:id", async function(request, response) {
+    try{
+        const user = await User.findByPk(request.params.id)
+        response.status(200).send({user})
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+
 
 module.exports = router
