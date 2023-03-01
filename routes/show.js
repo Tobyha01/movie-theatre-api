64d91501c2ba017ba637f7dd0d0955a4f7dcf947
Tobyha01 <tobyhamilton01@gmail.com>
2023-03-01 03:38:38 +0000

    Added two get (read) methods, one for finding all shows, a second one for finding a specific show using the shows's primary key in the request. Created a third get (read) method for finding shows of a particular genre, using the genre in the request endpoint, and passing it as a request parameter in the 'where' argument on the 'findAll()' method ({where: {genre: request.params.gnre}}). Added a put (update) method to update a shows's rating, where a show is asociated with a user, by using the 'userId' as the request endpoint. Added a put (update) method to update a specieid show's status, by using the show's 'id' as the request endpoint. Added a delete method to remove a specified show from the Show model (db), by using the show's 'id' as the request endpoint.

--- routes/show.js
+++ routes/show.js
@@ -6,4 +6,66 @@ const {Show} = require("../models/index")
 
 router.use(express.json())
 
+router.get("/", async function(request, response) {
+    try{
+        const shows = await Show.findAll()
+        response.status(200).send(shows)
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+router.get("/:id", async function (request, response){
+    try{
+        const show = await Show.findByPk(request.params.id) 
+        response.status(200).send(show)
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+router.get("/genre/:genre", async function(request, response) {
+    try{
+        const shows = await Show.findAll({where: {genre: request.params.genre}})
+        response.status(200).send(shows)
+    }
+    catch(error){
+        response.send(500).send({error: error.message})
+    }
+})
+
+router.put("/userId/:userId", async function(request, response) {
+    try{
+        const show = await Show.findOne({where: {userId: request.params.userId}})
+        await show.update({rating: request.body.rating})
+        response.status(200).send(show)
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+router.put("/:id", async function(request, response) {
+    try{
+        const show = await Show.findByPk(request.params.id)
+        show.update({status: request.body.status})
+        response.status(200).send(show)
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+router.delete("/:id", async function(request, response) {
+    try{
+        const show = await Show.destroy({where: {id: request.params.id}})
+        response.status(200).send({show})
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
 module.exports = router
